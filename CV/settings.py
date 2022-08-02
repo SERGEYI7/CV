b46f39ab8a3c9f4abd60a4d71b6beda993964e2e
Yakovlev <prog_03@omzit.ru>
2022-08-02 13:46:03 +0600

    delete secret key

--- CV/settings.py
+++ CV/settings.py
@@ -21,12 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-<<<<<<< HEAD
-=======
 with open(f'{BASE_DIR}/settings/secret_key.json', 'r') as file_key:
     KEY = json.load(file_key)['KEY']
 SECRET_KEY = KEY
->>>>>>> f698c97 (add app home)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
